c308c4c3f78944de7fefbfd06922fba542e5411c
南楠 <nannan@Max.local>
2023-11-13 13:53:28 +0800

    chore: push

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<!--
+ * @Author: NanNan
+ * @Date: 2023-11-13 11:10:57
+ * @LastEditTime: 2023-11-13 13:20:45
+ * @Description: 
+-->
+<script setup>
+import moment from 'moment';
+
+import HelloWorld from './components/HelloWorld.vue';
+
+console.log(moment().add(7, 'd'));
+</script>
+
+<template>
+  <div>
+    <a target="_blank" @click="toTopFunc">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: NanNan
+ * @Date: 2023-11-13 11:10:57
+ * @LastEditTime: 2023-11-13 13:18:47
+ * @Description:
+ */
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import 'moment/dist/locale/zh-cn'; //国际化更改 默认英文改成中文
+
+createApp(App).mount('#app');
